9e66f38e773e431c92f1c773be671fe2041448ba
Nicolas Buquet <nicolas.buquet@beta.gouv.fr>
2026-05-13 18:16:53 +0200

    Create TileView component

---
+++ Sources/DesignSystem/Swift/Components/TileView/TileView.swift
@@ -0,0 +1,49 @@
+//
+//  TileView.swift
+//  AMI-Design-System
+//
+//  Created by Nicolas Buquet on 13/05/2026.
+//
+
+import SwiftUI
+
+struct TileView: View {
+    @State var title: String
+    @State var content: String
+
+    var body: some View {
+        VStack {
+            Group {
+                VStack(alignment: .leading) {
+                    Text(title)
+                        .font(DsfrFont.h4.font)
+                        .foregroundStyle(AmiDesignSystem.Colors.Artwork.Major.blueFrance.swiftUIColor)
+                    Text(content)
+                        .font(DsfrFont.textSM.font)
+                    HStack {
+                        AmiDesignSystem.Icons.Arrows.arrowRightSLine.swiftUIImage
+                            .foregroundStyle(AmiDesignSystem.Colors.Artwork.Major.blueFrance.swiftUIColor)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                }
+                .padding(EdgeInsets(top: 16.0, leading: 16.0, bottom: 2.0, trailing: 16.0))
+            }
+            AmiDesignSystem.Colors.Border.Default.blueFrance.swiftUIColor
+                .frame(height: 8.0)
+        }
+        .border(AmiDesignSystem.Colors.Border.Default.blueFrance.swiftUIColor, width: 1.0)
+        .padding(16)
+        .frame(maxWidth: .infinity)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ScrollView {
+            ForEach(0..<10) { i in
+                TileView(title: "Tile #\(i)", content: "Tile content\nrunning on 2 lines")
+            }
+            .navigationTitle("Tile example")
+        }
+    }
+}
